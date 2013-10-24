f1b0995e705a59affb4d7f6dffc5ee44fe4f65a3
Xavier Dutoit <xavier@tttp.eu>
2013-10-24 16:46:22 +0200

    add option to take all folders

--- xml.php
+++ xml.php
@@ -1,19 +1,36 @@
 <?
-$file=$argv[1];
-$csv=substr ($argv[1],0,-3)."csv";
-$fp = fopen($csv, 'w');
-
-$xml = simplexml_load_file ($file);
 $keys= array ("country","postal.code","address","city","date.of.birth","lastname","nationality","firstname");
 $prefix = strlen("oct.property.");
-foreach ($xml->signatures->signature as $s) {
-  $result["date"]= substr($s->submissionDate,0,-6);
-  foreach ($s->signatoryInfo->groups->group as $g) {
-    group ($g,$result);
-  }
-  fputcsv($fp,$result);
+
+
+$file=$argv[1];
+
+if (!$file) {
+  die ("USAGE: \$php xml.php file.xml for a single file\n or \$php xml.php folder\n");
 }
+
+
+if (is_dir($file)) {
+  $total = 0;
+  $csv= $argv[1]."csv";
+  $fp = fopen($csv, 'w');
+  $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($file));
+  foreach($objects as $name => $object){
+    if ("_dec.xml" == substr ($name,-8)) {
+      $s = xml2csv($fp,$name);
+      $total +=$s;
+      echo "\n $s signatures in $name";
+    }
+  }
+} else {
+  $csv=substr ($argv[1],0,-3)."csv";
+  $fp = fopen($csv, 'w');
+  $total = xml2csv($fp,$file);
+}
+echo "\n $total signatures in total in file $fp\n";
 fclose($fp);
+
+
 
 function group($g,&$r) {
   global $keys,$prefix;
@@ -22,4 +39,18 @@ function group($g,&$r) {
       $r[substr($p->key,$prefix)] = (string)$p->value;
     }
   }
+}
+
+function xml2csv ($fp, $file) {
+  $total = 0;
+  $xml = simplexml_load_file ($file);
+  foreach ($xml->signatures->signature as $s) {
+    $total++;
+    $result["date"]= substr($s->submissionDate,0,-6);
+    foreach ($s->signatoryInfo->groups->group as $g) {
+      group ($g,$result);
+    }
+    fputcsv($fp,$result);
+  }
+  return $total;
 }
